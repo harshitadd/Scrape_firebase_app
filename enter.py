b304af3620349133d9bd933e778df34d2cdf75e7
Harshitadd <31439716+harshitadd@users.noreply.github.com>
2018-10-27 16:00:23 +0530

    jquery

---
+++ enter.py
@@ -0,0 +1,13 @@
+import test
+import fresh_tomatoes
+# toy_story = test.Movie("Toy Story","Story about a boy whose toys come to life","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+# #print(toy_story.storyline)
+# avatar = test.Movie("Avatar","Marine on alien planet","https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
+# #print(avatar.storyline)
+# #avatar.show_trailer()
+# alvin = test.Movie("Alvin and the Chipmunks","Chipmunks who can sing","https://upload.wikimedia.org/wikipedia/en/d/dc/Alvin_and_the_Chipmunks2007.jpg","https://www.youtube.com/watch?v=xA6cOSEZhzM")
+# school_of_rock = test.Movie("School Of Rock","Using rock music to learn","https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg","https://www.youtube.com/watch?v=3PsUJFEBC74")
+# harry_potter = test.Movie("Harry Potter","Magic school","https://upload.wikimedia.org/wikipedia/en/7/7a/Harry_Potter_and_the_Philosopher%27s_Stone_banner.jpg","https://www.youtube.com/watch?v=PbdM1db3JbY")
+# movies = [toy_story,avatar,alvin,school_of_rock,harry_potter]
+fresh_tomatoes.open_movies_page()
+#fresh_tomatoes.hello()
